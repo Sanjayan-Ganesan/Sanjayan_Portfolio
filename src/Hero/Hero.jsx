762ec1af3f15d4b29df08946f5635b749f7a4ef0
Sanjayan <sanjayanganesan@gmail.com>
2025-02-28 23:04:52 +0530

    Updated the hero section

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Hero.module.css';
 import hero from '../Assests/Software_Eng.jpeg';
+import {Link} from 'react-scroll';
 
 const Hero = () => {
   return (
